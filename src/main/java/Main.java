ea8e0fe23c79b426177f9b79c81fcd39797bea66
ddoubleo <sergienko.ni@edu.spbstu.ru>
2020-05-12 16:07:08 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static String input;
+    public static void main(String args[]) throws IOException {
+        Coder.decode2();
+    }
+}
