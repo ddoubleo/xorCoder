fa74af788fd7c9b969e9c8d89d66b1628e4fad4f
ddoubleo <sergienko.ni@edu.spbstu.ru>
2020-05-13 18:04:04 +0300

    Minor changes

--- src/main/java/Coder.java
+++ src/main/java/Coder.java
@@ -6,32 +6,20 @@ import java.util.Scanner;
 public class Coder {
     public static String input;
 
-    /*static public void decode(String key, String filename) throws IOException {
-        int nextBit = 0;
-        int decimalKey = Integer.parseInt(key,16);
-        FileInputStream inputStream = new FileInputStream("C:\\Projects\\Coder\\src\\Resources\\InputText");
-        FileOutputStream outputStream = new FileOutputStream("C:\\Projects\\Coder\\src\\Resources\\OutputText");
-        while (nextBit != -1) {
-            nextBit = inputStream.read();
-            outputStream.write(nextBit^decimalKey);
-        }
-        outputStream.close();
-        inputStream.close();
-    }*/
-    static public void decode2() throws IOException {
+    static public void decode() throws IOException {
         String inputPath = "";
         String outputPath = "";
         Scanner sc = new Scanner(System.in);
         System.out.println("Specify the input path:");
-        inputPath = sc.nextLine();
+        inputPath = sc.nextLine().replace("\\", "\\\\");
+        System.out.println(inputPath);
         System.out.println("Specify the output path:");
-        outputPath = sc.nextLine();
+        outputPath = sc.nextLine().replace("\\", "\\\\");
         do {
             input = sc.nextLine();
             String[] splitted = input.split(" ");
             if (splitted[0].equals("ciphxor")) {
                 int decimalKey = Integer.parseInt(splitted[2], 16);
-                //splitted[3] = splitted[3].substring(0,splitted[3].length()-4);
                 if (splitted.length != 6) {
                     splitted = Arrays.copyOf(splitted, 6);
                     splitted[5] = splitted[3];
@@ -44,12 +32,11 @@ public class Coder {
                         = new PrintWriter(new BufferedWriter(new FileWriter(
                         output)));
                 String inputLine = in.readLine();
-                String outputLine = "";
                 do {
-                    StringBuilder sb = new StringBuilder();
+                    StringBuilder outputLine = new StringBuilder();
                     for (int j = 0; j < inputLine.length(); j++)
-                        sb.append(Character.toString((char) (inputLine.charAt(j) ^ decimalKey)));
-                    out.println(sb.toString());
+                        outputLine.append(Character.toString((char) (inputLine.charAt(j) ^ decimalKey)));
+                    out.println(outputLine.toString());
                 } while ((inputLine = in.readLine()) != null);
                 out.close();
                 in.close();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,10 @@
 
 
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static String input;
     public static void main(String args[]) throws IOException {
-        Coder.decode2();
+        Coder.decode();
     }
 }
