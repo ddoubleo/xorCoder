21a1e0e782fb893acbdfe506a4f4a81b1cd9362d
ddoubleo <sergienko.ni@edu.spbstu.ru>
2020-06-09 21:07:17 +0300

    fixed some bugs, tests now work properly

--- src/main/java/Coder.java
+++ src/main/java/Coder.java
@@ -6,7 +6,7 @@ public class Coder {
 
     static public void decode(String inputRoute, String key, String outputRoute) throws IOException {
         if (!key.matches("[a-f\\d]*")) {
-            throw new IllegalArgumentException("Wrong key: "+key);
+            throw new IllegalArgumentException("Wrong key: " + key);
         } else {
             BufferedReader in
                     = new BufferedReader(new FileReader(inputRoute));
@@ -14,9 +14,13 @@ public class Coder {
                     = new PrintWriter(new BufferedWriter(new FileWriter(new File(outputRoute))));
             String inputLine = in.readLine();
             do {
+                int count = 0;
                 StringBuilder outputLine = new StringBuilder();
-                for (int j = 0; j < inputLine.length(); j++)
-                    outputLine.append(Character.toString((char) (inputLine.charAt(j) ^ key.charAt(j % key.length()))));
+                for (int j = 0; j < inputLine.length(); j++) {
+                    outputLine.append(Character.toString((char) (inputLine.charAt(j) ^ key.charAt(count))));
+                    count++;
+                    if (count==key.length()) count=0;
+                }
                 out.println(outputLine.toString());
             } while ((inputLine = in.readLine()) != null);
             out.close();
--- src/test/java/CoderTest.java
+++ src/test/java/CoderTest.java
@@ -13,25 +13,25 @@ public class CoderTest {
     @Test
     public void testKeyChecker() throws IOException {
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Coder.decode("C:\\Users\\serni\\Desktop\\Encode.txt",
-                    "C:\\Users\\serni\\Desktop\\Encoded.txt", "abch");
+            Coder.decode("src/main/resources/input/Encode.txt",
+                    "3abfk","src/main/resources/output/Encoded.txt");
         });
     }
     @Test
     public void fileContentChecker1() throws IOException{
-        Coder.decode("C:\\Users\\serni\\Desktop\\Encode.txt",
-                     "223","C:\\Users\\serni\\Desktop\\Encoded.txt");
-        File file1 = new File("C:\\Users\\serni\\Desktop\\Encoded.txt");
-        File file2 = new File("C:\\Users\\serni\\Desktop\\Template.txt");
+        Coder.decode("src/main/resources/input/Encode.txt",
+                     "3abf","src/main/resources/output/Encoded.txt");
+        File file1 = new File("src/main/resources/output/Encoded.txt");
+        File file2 = new File("src/main/resources/output/Template.txt");
         //Assertions.assertEquals(file1,file2);
         assertTrue(FileUtils.contentEquals(file1, file2));
     }
     @Test
     public void fileContentChecker2() throws IOException{
-        Coder.decode("C:\\Users\\serni\\Desktop\\Encode1.txt",
-                "3abf","C:\\Users\\serni\\Desktop\\Encoded1.txt");
-        File file1 = new File("C:\\Users\\serni\\Desktop\\Encoded1.txt");
-        File file2 = new File("C:\\Users\\serni\\Desktop\\Template1.txt");
+        Coder.decode("src/main/resources/input/Encode1.txt",
+                "1453a","src/main/resources/output/Encoded1.txt");
+        File file1 = new File("src/main/resources/output/Encoded1.txt");
+        File file2 = new File("src/main/resources/output/Template1.txt");
         //Assertions.assertEquals(file1,file2);
         assertTrue(FileUtils.contentEquals(file1, file2));
     }
